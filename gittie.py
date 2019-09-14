e245f8cce1bde8bc414614b00e6d9aed6f3fc960
MissMonia <klonowska@yahoo.com>
2019-09-14 22:16:03 +0200

    shout function

--- gittie.py
+++ gittie.py
@@ -1,13 +1,18 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
+
 introduce()
+
 
 def joke():
     print("Morawiecki tu byl")
 
+
 def add(a, b):
     return a+b
 
+
 def shout():
-    print("I'm shouting")
+    print("Woaaaaaaa!")
+
